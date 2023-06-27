5bddb9dd02035e164596874204914210b2827765
marcostota <juniorvans2@gmail.com>
2023-06-27 15:18:13 -0300

    add haburger project with vuejs

---
+++ Vue/hamburguer/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="footer">
+    <p>Make Your Burger &copy; 2021</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+#footer {
+  height: 200px;
+  width: 100%;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #fcba03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ Vue/hamburguer/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Make your burger:</h1>
+      <BurgerForm />
+    </div>
+  </div>
+</template>
+
+
+<script>
+import BurgerForm from "@/components/BurgerForm.vue";
+import Banner from "../components/Banner.vue";
+
+export default {
+  name: "HomeView",
+  components: { Banner, BurgerForm },
+};
+</script>
